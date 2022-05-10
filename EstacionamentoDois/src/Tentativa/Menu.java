f71c068a33eb7ff031a310307dbe7e4af946f633
Eduardo Longo <65884093+edulongodevgeo@users.noreply.github.com>
2022-05-10 17:56:23 -0300

    Alterações do dia 10/05/2022 - MVP_02

--- EstacionamentoDois/src/Tentativa/Menu.java
+++ EstacionamentoDois/src/Tentativa/Menu.java
@@ -1,118 +1,140 @@
 package Tentativa;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
+	static float valorDoDia = 0;
+	int sum = 0;
 
 	public static void main(String[] args) {
-
 		Vaga[] vagas = new Vaga[50];
+		List<Vaga> relatorioVg = new ArrayList<Vaga>();
+		List<Veiculo> relatorioVc = new ArrayList<Veiculo>();
+		List<Date> lista2 = new ArrayList<Date>();
 		Integer opcao = -1;
+		int taxaPorHora = 2;
+
 		Scanner sc = new Scanner(System.in);
-
 		while (opcao != 0) {
-			System.out.println("\n ____Bem vindo ao FourParking____" 
-					+ "\n|________________________________|"
-					+ "\n|1 - Consultar vagas ocupadas    |" 
-					+ "\n|2 - Consultar vagas disponíveis |"
-					+ "\n|3 - Colocar carro em vaga       |" 
-					+ "\n|4 - Retirar carro em vaga       |"
-					+ "\n|5 - Dados da vaga               |"
-					+ "\n|--------------------------------|" 
-					+ "\n|        0 - S A I R             |"
-					+ "\n|________________________________|"
-					+ "\n"
-					+ "\nInforme a operação desejada >>>");
-
+			System.out.println("\n Bem vindo ao FourParking" + "\n|________________________________|"
+					+ "\n|1 - Consultar vagas ocupadas |" + "\n|2 - Consultar vagas disponíveis |"
+					+ "\n|3 - Colocar carro em vaga |" + "\n|4 - Retirar carro em vaga |" + "\n|5 - Dados da vaga |"
+					+ "\n|6 - Relatório |" + "\n|--------------------------------|" + "\n| 0 - S A I R |"
+					+ "\n|________________________________|" + "\n" + "\nInforme a operação desejada >>>");
 			opcao = sc.nextInt();
-			
 			switch (opcao) {
 			case 1:
-				
 				String vagasDisponiveis = "\n\nAs vagas ocupadas são: \n";
-				
-				for(int i = 1; i < vagas.length; i++) {
+				for (int i = 0; i < vagas.length; i++) {
 					if (vagas[i] != null) {
-						int vagaCorreta = i;
+						int vagaCorreta = i + 1;
 						vagasDisponiveis += vagaCorreta + ", ";
 					}
 				}
 				System.out.println(vagasDisponiveis);
 				break;
-				
 			case 2:
 				String vagasOcupadas = "\n\nAs vagas disponíveis são: \n";
-
-				for (int i = 1; i < vagas.length; i++) {
+				for (int i = 0; i < vagas.length; i++) {
 					if (vagas[i] == null) {
-						int vagaCorreta = i;
-						vagasOcupadas += vagaCorreta + ", ";
+						int vagaCorreta = i + 1;
+						vagasOcupadas += vagaCorreta + ",";
 					}
 				}
 				System.out.println(vagasOcupadas);
 				break;
-				
 			case 3:
-				System.out.println("Digite a placa do carro: ");
-				String placa = sc.next();
-				System.out.println("Digite o modelo do veículo: ");
-				String modelo = sc.next();
-				
-				Veiculo veiculo = new Veiculo();
-				veiculo.setModelo(modelo);
-				veiculo.setPlaca(placa);
-				
-				Date d = new Date();
-				
-				Vaga vaga = new Vaga();
-				vaga.setHoraEntrada(d);
-				vaga.setVeiculo(veiculo);
-				
 				System.out.println("Digite a vaga que será ocupada: ");
-				int i = sc.nextInt();
-				
-				vagas[i] = vaga;
-				
+				int i = sc.nextInt() - 1;
+				if (vagas[i] == null) {
+					System.out.println("Digite a placa do carro: ");
+					String placa = sc.next();
+					System.out.println("Digite o modelo do veículo: ");
+					String modelo = sc.next();
+					Veiculo veiculo = new Veiculo();
+					veiculo.setModelo(modelo);
+					veiculo.setPlaca(placa);
+					Date d = new Date();
+					Vaga vaga = new Vaga();
+					vaga.setHoraEntrada(d);
+					vaga.setVeiculo(veiculo);
+
+					System.out.println("Vaga Cadastrada com Sucesso");
+					vagas[i] = vaga;
+					relatorioVg.add(vaga);
+					relatorioVc.add(veiculo);
+				} else {
+					System.out.println("Vaga em Uso");
+				}
+
 				break;
-				
+
 			case 4:
 				System.out.println("Digite a vaga que será desocupada: ");
-				
-				int x = sc.nextInt();
-				
-				if(vagas[x] == null) {
+				int x = sc.nextInt() - 1;
+				if (vagas[x] == null) {
 					System.out.println("Não existem carros nesta vaga.");
 				} else {
+					Date entrada = vagas[x].getHoraEntrada();
+					Date saida = new Date();
+					Calendar dataEntrada = Calendar.getInstance();
+					dataEntrada.setTime(entrada);
+					Calendar dataSaida = Calendar.getInstance();
+					dataSaida.setTime(saida);
+					int horas = dataSaida.get(Calendar.SECOND) - dataEntrada.get(Calendar.SECOND);
+					int valorAPagar = horas * taxaPorHora;
+					valorDoDia += valorAPagar;
+					lista2.add(saida);
 					vagas[x] = null;
-					
-					System.out.println("Vaga desocupada com sucesso!");					
+					System.out.println("Vaga desocupada com sucesso!");
+					System.out.println("\nEntrada do veículo: " + entrada);
+					System.out.println("Saída do veículo: " + saida);
+					System.out.println("Valor total a pagar: R$ " + valorAPagar);
 				}
 				break;
 			case 5:
 				System.out.println("Digite a vaga que quer saber os dados: ");
-				int y = sc.nextInt();
-				
-				if(vagas[y] == null) {
+				int y = sc.nextInt() - 1;
+				if (vagas[y] == null) {
 					System.out.println("Não existem carros nesta vaga.");
 				} else {
-					vagas[y] = null;
-					
-					System.out.println(vagas[y]);
+					Vaga vaga5 = vagas[y];
+					String s = "Dados da vaga: ";
+					s += "\nHorário da entrada: " + vaga5.getHoraEntrada();
+					s += "\nModelo do veículo: " + vaga5.getVeiculo().getModelo();
+					s += "\nPlaca do veículo: " + vaga5.getVeiculo().getPlaca();
+					System.out.println(s);
 				}
-				
 				break;
-				
+			case 6:
+
+				System.out.println("Relatório: ");
+				System.out.println();
+				for (int a = 0; a < relatorioVc.size(); a++) {
+					int b = a + 1;
+					System.out.println();
+					System.out.println("Carro " + b + ":");
+					System.out.println("horario entrada: " + relatorioVg.get(a).getHoraEntrada());
+					System.out.println("Modelo: " + relatorioVc.get(a).getModelo());
+					System.out.println("Placa: " + relatorioVc.get(a).getPlaca());
+					System.out.println("horario saida: " + lista2.get(a));
+				}
+				break;
 			default:
 				if (opcao != 0) {
 					System.out.println("\n\nVocê precisa selecionar uma das opções válidas\n");
 				}
 				break;
 			}
-			}
+		}
+		System.out.println("Total ganho no dia: R$ " + valorDoDia + ",00");
 
 		if (sc != null) {
-			sc.close();			
+			sc.close();
 		}
 	}
 }
